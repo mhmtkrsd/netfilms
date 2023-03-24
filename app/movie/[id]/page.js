37bebf932659208ca461e1b80f8195e8b4cf17d3
Mehmet Kürşad Yuca <mehmetkursadyuca@gmail.com>
2023-03-24 18:43:58 +0300

    Movie detail page

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,22 @@
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function MoviePage({ params, searchParams }) {
+  await delay(1000);
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id.toString() === params.id
+  );
+  if (!movieDetail) {
+    notFound();
+  }
+  if (searchParams.error === "true") {
+    throw new Error("Error happened!");
+  }
+  return <MovieContainer movie={movieDetail} />;
+}
+export default MoviePage;
